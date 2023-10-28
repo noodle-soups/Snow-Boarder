54b544464d84afc2dd78a367233865696cf4ff22
Martin Mallon <88795187+noodle-soups@users.noreply.github.com>
2023-10-28 15:22:08 +0100

    commit

--- Assets/CrashDetector.cs
+++ Assets/CrashDetector.cs
@@ -1,16 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CrashDetector : MonoBehaviour
 {
-
 
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Ground")
         {
             Debug.Log("Game over!");
+            SceneManager.LoadScene(0);
         }
     }
+
 }
