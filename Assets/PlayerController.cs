941d4be8d92d69b4673b5ced5f104736085da988
Martin Mallon <88795187+noodle-soups@users.noreply.github.com>
2023-10-30 14:41:51 +0000

    Update PlayerController.cs

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,23 +8,45 @@ public class PlayerController : MonoBehaviour
     
     [SerializeField] float torqueAmount = 1f;
     Rigidbody2D rb2d;
+    SurfaceEffector2D surfaceEffector2D;
+    [SerializeField] float speedBase = 12f;
+    [SerializeField] float speedBoost = 20f;
 
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        surfaceEffector2D = FindAnyObjectByType<SurfaceEffector2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey(KeyCode.LeftArrow))
-        {
-            rb2d.AddTorque(torqueAmount);
-        }
+        RotatePlayer();
+        RespondToBoost();
+    }
 
-        else if(Input.GetKey(KeyCode.RightArrow))
+    void RespondToBoost()
+    {
+        if(Input.GetKey(KeyCode.Space))
+        {
+            surfaceEffector2D.speed = speedBoost;
+        }
+        else 
+        {
+            surfaceEffector2D.speed = speedBase;
+        }
+    }
+
+    void RotatePlayer()
+    {
+        if (Input.GetKey(KeyCode.D))
         {
             rb2d.AddTorque(-torqueAmount);
         }
+
+        else if (Input.GetKey(KeyCode.A))
+        {
+            rb2d.AddTorque(torqueAmount);
+        }
     }
+
 }
