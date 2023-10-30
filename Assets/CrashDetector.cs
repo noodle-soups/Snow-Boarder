bdec3ea08a88dde9e11ebb3c8be8fa9291a26228
Martin Mallon <88795187+noodle-soups@users.noreply.github.com>
2023-10-30 16:56:22 +0000

    commit

--- Assets/CrashDetector.cs
+++ Assets/CrashDetector.cs
@@ -8,12 +8,14 @@ public class CrashDetector : MonoBehaviour
 
     [SerializeField] float reloadDelay = 0.5f;
     [SerializeField] ParticleSystem crashEffect;
+    bool hasCrashed = false;
 
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.tag == "Ground")
+        if (other.tag == "Ground" && !hasCrashed)
         {
+            hasCrashed = true;
             FindObjectOfType<PlayerController>().DisableControls();
             Debug.Log("Game over!");
             crashEffect.Play();
